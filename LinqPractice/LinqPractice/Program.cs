af1e71aac83ab84189abad91f35f9c273c5b185f
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-03-18 16:56:18 +0100

    First task done

--- LinqPractice/LinqPractice/Program.cs
+++ LinqPractice/LinqPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace LinqPractice
 {
@@ -6,7 +7,24 @@ namespace LinqPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //BasicLinqOperations();
+
+            var employees = Employee.GetEmployees();
+            //Select women employees
+            var women = employees.Where(x=>x.Gender == Gender.Woman);
+
+            //Select employees with seniority greater than 8 years
+
+        }
+
+        private static void BasicLinqOperations()
+        {
+            var ages = new[] {42, 12, 32, 45};
+
+            Console.WriteLine(ages.Min());
+            Console.WriteLine(ages.Max());
+            Console.WriteLine(ages.Sum());
+            Console.WriteLine(ages.Average());
         }
     }
 }
