7e1eb0556db97015cae7e22c83cbabb1adef18ae
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-03-25 16:08:19 +0100

    Work in progress

---
+++ UnitTesting/UnitTestsExample/Automotive.Tests/CarTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Automotive.Tests
+{
+    public class CarTests
+    {
+        [Fact]
+        public void Can_create_a_valid_car_and_its_properties_are_correct()
+        {
+
+        }
+    }
+}
