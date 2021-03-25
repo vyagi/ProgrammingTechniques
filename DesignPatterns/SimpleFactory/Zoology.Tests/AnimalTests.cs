eefaea523b6a773e5862e448d0a53bf534ed5af1
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-03-25 17:51:13 +0100

    Generic factory

--- DesignPatterns/SimpleFactory/Zoology.Tests/AnimalTests.cs
+++ DesignPatterns/SimpleFactory/Zoology.Tests/AnimalTests.cs
@@ -8,18 +8,18 @@ namespace Zoology.Tests
         [Fact]
         public void Factory_returns_instance_of_correct_concrete_type()
         {
-            var cat = new AnimalFactory().Create("Cat");
-            var dog = new AnimalFactory().Create("Dog");
-            var fish = new AnimalFactory().Create("Fish");
-            var monkey = new AnimalFactory().Create("Monkey");
-            var unknown = new AnimalFactory().Create("Not exsistent");
+            var cat = new AnimalFactory().Create<Cat>();
+            var dog = new AnimalFactory().Create<Dog>();
+            var fish = new AnimalFactory().Create<Fish>();
+            var monkey = new AnimalFactory().Create<Monkey>();
+            //var unknown = new AnimalFactory<XXX>().Create();
 
             cat.Should().BeOfType<Cat>();
             dog.Should().BeOfType<Dog>();
             fish.Should().BeOfType<Fish>();
             monkey.Should().BeOfType<Monkey>();
 
-            unknown.Should().BeNull();
+            //unknown.Should().BeNull();
         }
     }
 }
--- DesignPatterns/SimpleFactory/Zoology/AnimalFactory.cs
+++ DesignPatterns/SimpleFactory/Zoology/AnimalFactory.cs
@@ -4,11 +4,13 @@ namespace Zoology
 {
     public class AnimalFactory
     {
-        public DomesticAnimal Create(string type)
-        {
-            var actualType = Type.GetType($"Zoology.{type}");
-            return actualType == null ? null : (DomesticAnimal) Activator.CreateInstance(actualType);
-        }
+        public DomesticAnimal Create<T>() where T : DomesticAnimal, new() => new T();
+
+        // public DomesticAnimal Create(string type)
+        // {
+        //     var actualType = Type.GetType($"Zoology.{type}");
+        //     return actualType == null ? null : (DomesticAnimal) Activator.CreateInstance(actualType);
+        // }
 
         // public DomesticAnimal Create(string type) =>
         //     type switch
