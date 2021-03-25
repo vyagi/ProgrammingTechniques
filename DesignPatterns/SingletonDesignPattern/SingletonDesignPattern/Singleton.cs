8581700354146700ab6a426ef0b9b06edf272497
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-03-25 17:12:30 +0100

    Version 3

--- DesignPatterns/SingletonDesignPattern/SingletonDesignPattern/Singleton.cs
+++ DesignPatterns/SingletonDesignPattern/SingletonDesignPattern/Singleton.cs
@@ -5,16 +5,20 @@ namespace SingletonDesignPattern
     public class Singleton
     {
         private static Singleton _instance;
-        
+
+        private static object padLock = new object();
+
         public DateTime CreatedAt { get; private set;  }
 
         private Singleton() => CreatedAt = DateTime.Now;
 
         public static Singleton GetInstance()
         {
-            if (_instance == null) 
-                _instance = new Singleton();
-
+            lock (padLock)
+            {
+                if (_instance == null)
+                    _instance = new Singleton();
+            }
             return _instance;
         }
     }
