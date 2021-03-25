3c2ecdc81dd727f1bded197e2e05296c786d3559
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-03-25 17:36:39 +0100

    Simple factory approach one

--- DesignPatterns/SimpleFactory/Zoology.Tests/AnimalTests.cs
+++ DesignPatterns/SimpleFactory/Zoology.Tests/AnimalTests.cs
@@ -1,8 +1,22 @@
-﻿using System;
+﻿using FluentAssertions;
+using Xunit;
 
 namespace Zoology.Tests
 {
     public class AnimalTests
     {
+        [Fact]
+        public void Factory_returns_instance_of_correct_concrete_type()
+        {
+            var cat = new AnimalFactory().Create("Cat");
+            var dog = new AnimalFactory().Create("Dog");
+            var fish = new AnimalFactory().Create("Fish");
+            var unknown = new AnimalFactory().Create("Not exsistent");
+
+            cat.Should().BeOfType<Cat>();
+            dog.Should().BeOfType<Dog>();
+            fish.Should().BeOfType<Fish>();
+            unknown.Should().BeNull();
+        }
     }
 }
---
+++ DesignPatterns/SimpleFactory/Zoology/AnimalFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Zoology
+{
+    public class AnimalFactory
+    {
+        public DomesticAnimal Create(string type)
+        {
+            if (type == "Dog")
+            {
+                return new Dog();
+            }
+
+            if (type == "Cat")
+            {
+                return new Cat();
+            }
+
+            if (type == "Fish")
+            {
+                return new Fish();
+            }
+
+            return null; //Bad idea in general
+        }
+    }
+}
