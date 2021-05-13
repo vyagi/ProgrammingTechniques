2390020b85abfa623d66dfa5e5cde7d3bbdbeaf7
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-05-13 17:02:10 +0200

    Ef database first

--- Orm/EfDatabaseFirst/Program.cs
+++ Orm/EfDatabaseFirst/Program.cs
@@ -17,3 +17,26 @@ using var context = new AdventureWorksContext();
 
 // Console.WriteLine(context.Customers.Count());
 
+// var customer = context.Customers.Single(x=>x.CustomerId == 2);
+// Console.WriteLine($"{customer.LastName} {customer.FirstName} {customer.CompanyName}");
+//
+// customer.CompanyName = "University of Information Technology and Management";
+//
+// Console.WriteLine($"{customer.LastName} {customer.FirstName} {customer.CompanyName}");
+//
+// context.SaveChanges(); 
+
+// var newAddress = new Address();
+// newAddress.AddressLine1 = "Sucharskiego 2";
+// newAddress.City = "Rzeszów";
+// newAddress.CountryRegion = "Podkarpackie";
+// newAddress.StateProvince = "Podkarpackie";
+// newAddress.PostalCode = "35-330";
+// context.Addresses.Add(newAddress);
+//
+// context.SaveChanges();
+
+var addressToDelete = context.Addresses.First(x=>x.City=="Rzeszów");
+context.Addresses.Remove(addressToDelete);
+
+context.SaveChanges();
