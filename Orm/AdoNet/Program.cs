abc2cc8cba01645cc72392980fda2195cc14e7f1
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-05-13 16:48:20 +0200

    Databasefirst done

---
+++ Orm/AdoNet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+var connectionString =
+    "Server=tcp:orderspocdbserver.database.windows.net,1433;Initial Catalog=AdventureWorks;Persist Security Info=False;User ID=marcin.jagiela;Password=Qwer!234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+var queryString = @"
+SELECT * FROM SalesLT.Product p
+JOIN SalesLT.ProductCategory pc
+ON pc.ProductCategoryID = p.ProductCategoryID";
+
+await using var connection = new SqlConnection(connectionString);
+connection.Open();
+
+await using var command = new SqlCommand(queryString, connection);
+
+command.CommandType = CommandType.Text;
+var reader = await command.ExecuteReaderAsync();
+
+while (reader.Read()) 
+    Console.WriteLine(reader.GetInt32(0));
+
+
---
+++ Orm/EfDatabaseFirst/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using EfDatabaseFirst;
+using Microsoft.EntityFrameworkCore;
+
+using var context = new AdventureWorksContext();
+
+// foreach (var product in context.Products.Include(x=>x.ProductCategory))
+// {
+//     Console.WriteLine($"{product.Name} of category {product.ProductCategory.Name}");
+// }
+
+// foreach (var customer in context.Customers)
+// {
+//     Console.WriteLine($"{customer.LastName} {customer.FirstName}");
+// }
+
+// Console.WriteLine(context.Customers.Count());
+
